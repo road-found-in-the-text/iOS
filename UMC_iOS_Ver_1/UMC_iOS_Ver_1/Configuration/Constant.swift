dbb74146f3a6dc7400dd2275e148e932ce710d5f
shintaewon <53418280+shintaewon@users.noreply.github.com>
2023-01-09 19:26:27 +0900

    Add files via upload
    init: 프로젝트 초기설정 및 스토리보드 레퍼런스 생성

---
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Configuration/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  UMC_iOS_Ver_1
+//
+//  Created by 신태원 on 2023/01/07.
+//
+
+import Foundation
+
+//통신에 필요한 static 상수들
+struct Constant {
+    static let BASE_URL = "https://gateway.easytask.co.kr"
+    static var JWT = " "
+}
