03db5eb8625b16e026e12956991e59f75d32498e
Seoyoung Lee <wow9176@naver.com>
2023-01-12 23:50:24 +0900

    feat: 색상 asset 추가 및 UIImageView extension 이미지 색상 설정 메소드 추가

--- UMC_iOS_Ver_1/UMC_iOS_Ver_1/Util/Extension/UIImageView.swift
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Util/Extension/UIImageView.swift
@@ -20,4 +20,10 @@ extension UIImageView {
             }
         }
     }
+    
+    func setImageColor(color: UIColor) {
+      let templateImage = self.image?.withRenderingMode(.alwaysTemplate)
+      self.image = templateImage
+      self.tintColor = color
+    }
 }
