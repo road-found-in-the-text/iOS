e6864ad43c9f6764e0eab0f7845223544d9d3022
Seoyoung Lee <wow9176@naver.com>
2023-01-17 21:15:48 +0900

    #2 feat: Script Record 화면 UI 구현

---
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/ScriptPractice/ScriptRecord/Cell/ScriptRecordChartCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ScriptRecordChartCollectionViewCell.swift
+//  UMC_iOS_Ver_1
+//
+//  Created by 이서영 on 2023/01/17.
+//
+
+import UIKit
+
+import Charts
+
+class ScriptRecordChartCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var resultChart: RadarChartView!
+}
