6dffa899ac66e933e759974fa75d1ea399958eab
kingjeongkong <wjdqls9203@naver.com>
2023-01-18 12:27:06 +0900

    #1 [chore] Alamofire 라이브러리 설치

---
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/Home/HomeController/ReportViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ReportViewController.swift
+//  UMC_iOS_Ver_1
+//
+//  Created by 왕정빈 on 2023/01/17.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+}
