c4395c8d3ffb4040d86853a63addf5d0619b7631
Seoyoung Lee <wow9176@naver.com>
2023-01-18 13:51:35 +0900

    #3 feat: Script Practice 대본 슬라이드 효과 구현

---
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/ScriptPractice/Script/ScriptViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ScriptPracticeViewController.swift
+//  UMC_iOS_Ver_1
+//
+//  Created by 이서영 on 2023/01/18.
+//
+
+import UIKit
+
+class ScriptViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet var collectionView: UICollectionView!
+    
+    private let cellSize = CGSize(width: 302, height: 140)
+    private var minimumItemSpacing: CGFloat = 20
+    private let cellIdentifier = "scriptPTcell"
+    
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureCollectionView()
+    }
+    
+    func configureCollectionView() {
+        let cellWidth: CGFloat = floor(cellSize.width)
+        let insetX = (view.bounds.width - cellWidth) / 2.0
+        
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: insetX, bottom: 0, right: insetX)
+    }
+
+}
+
+// MARK: - UICollectionView
+extension ScriptViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? ScriptPTCollectionViewCell else {
+            assert(false)
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return cellSize
+    }
+    
+    // MARK: Paging Effect
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let cellWidthIncludeSpacing = cellSize.width + minimumItemSpacing
+        
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludeSpacing
+        let roundedIndex: CGFloat = round(index)
+        
+        offset = CGPoint(x: roundedIndex * cellWidthIncludeSpacing - scrollView.contentInset.left, y: scrollView.contentInset.top)
+        targetContentOffset.pointee = offset
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return minimumItemSpacing
+    }
+    
+}
