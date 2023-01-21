a2ab007c5ca90d20cab66f182ea3b2323a4a2b92
kingjeongkong <wjdqls9203@naver.com>
2023-01-21 11:42:33 +0900

    #1 [chore] 아이콘 이미지들 변경

--- UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/Home/HomeController/PostViewController.swift
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/Home/HomeController/PostViewController.swift
@@ -12,7 +12,7 @@ class PostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "exclamationmark.circle"), style: .plain, target: self, action: #selector(reportTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "ic_more"), style: .plain, target: self, action: #selector(reportTapped))
     }
     
     @objc func reportTapped() {
