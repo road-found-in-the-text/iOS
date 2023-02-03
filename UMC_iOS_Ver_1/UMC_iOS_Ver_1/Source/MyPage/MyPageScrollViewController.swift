ea744abd4cbc2d620eb2b78212d44647af024ac5
Seoyoung Lee <wow9176@naver.com>
2023-02-03 23:47:56 +0900

    #15 feat: navigation controller 생성 및 navigation bar 커스텀

--- UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/MyPage/MyPageScrollViewController.swift
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/MyPage/MyPageScrollViewController.swift
@@ -16,9 +16,28 @@ class MyPageScrollViewController: HPScrollViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         headerView = profileView
-        headerHeight = 180
+        headerHeight = 180 + (navigationController?.navigationBar.frame.height ?? 40)
+        
+        configureNavigationBar()
+    }
+    
+    func configureNavigationBar() {
+        let notificationBarButtonItem = UIBarButtonItem(image: UIImage(named: "ic_bell"), style: .plain, target: self, action: #selector(notificationBarButtonItemTapped))
+        let settingBarButtonItem = UIBarButtonItem(image: UIImage(named: "ic_settings"), style: .plain, target: self, action: #selector(settingBarButtonItemTapped))
+        
+        notificationBarButtonItem.tintColor = UIColor(named: "Main")
+        settingBarButtonItem.tintColor = UIColor(named: "Main")
+        
+        self.navigationItem.rightBarButtonItems = [settingBarButtonItem, notificationBarButtonItem]
+    }
+    
+    @objc func notificationBarButtonItemTapped() {
+        print("알림창")
+    }
+    
+    @objc func settingBarButtonItemTapped() {
+        print("설정창")
     }
 
 }
