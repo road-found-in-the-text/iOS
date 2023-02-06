2104987cbccfb2fd8bd8157d03592f270dba7973
Seoyoung Lee <wow9176@naver.com>
2023-02-06 14:14:31 +0900

    #22 feat: setting table view 헤더 생성

--- UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/MyPage/Setting/SettingViewController.swift
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/MyPage/Setting/SettingViewController.swift
@@ -8,20 +8,51 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    private let sectionTitle = ["내 계정", "앱 설정", "글에서 발견한 길 응원하기", "약관 및 정책", "게정 관리"]
+    private let sectionHeaderIdentifier = "SectionHeader"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureNavigationBar()
+        configureTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureNavigationBar() {
+        self.navigationItem.title = "설정"
     }
-    */
+    
+    func configureTableView() {
+        let sectionHeader = UINib(nibName: "SettingHeaderView", bundle: nil)
+        tableView.register(sectionHeader, forHeaderFooterViewReuseIdentifier: sectionHeaderIdentifier)
+    }
 
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section != 4 ? 2 : 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: sectionHeaderIdentifier) as? SettingHeaderView else {
+            return UIView()
+        }
+        header.titleLabel.text = sectionTitle[section]
+        
+        return header
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    
 }
---
+++ UMC_iOS_Ver_1/UMC_iOS_Ver_1/Source/MyPage/Setting/View/SettingHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingHeaderView.swift
+//  UMC_iOS_Ver_1
+//
+//  Created by 이서영 on 2023/02/06.
+//
+
+import UIKit
+
+class SettingHeaderView: UITableViewHeaderFooterView {
+    
+    @IBOutlet var titleLabel: UILabel!
+
+}
